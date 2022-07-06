799bced0dac0a7fea731326dcc27102493fd5856
Karen Siu <k3tang@gmail.com>
2022-07-05 17:43:57 -0700

    add piece

--- Chess_Game/rook.rb
+++ Chess_Game/rook.rb
@@ -3,8 +3,11 @@ require_relative "piece.rb"
 class Rook < Piece
     def initialize(color, position)
         super(color, position)
-        if color == :W
-            @symbol = 
+            if color == :W
+                @symbol = 
+            elsif color == :B 
+                @symbol = 
+            end 
     end 
 
 end 
